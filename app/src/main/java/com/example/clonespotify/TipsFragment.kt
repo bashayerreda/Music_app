76b35fa83debd26a04b037a13e3ae8cbd7dd8692
bashayerreda <bashayerreda79@gmail.com>
2022-07-18 20:02:05 +0200

    fix some issues and add biometric for security

--- app/src/main/java/com/example/clonespotify/TipsFragment.kt
+++ app/src/main/java/com/example/clonespotify/TipsFragment.kt
@@ -1,14 +1,21 @@
 package com.example.clonespotify
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.biometric.BiometricConstants.ERROR_USER_CANCELED
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.clonespotify.adapters.onItemBoardingAdapters
 import kotlinx.android.synthetic.main.fragment_tips.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 
 class TipsFragment : Fragment() {
     lateinit var onItemBoardingAdapters: onItemBoardingAdapters
@@ -21,6 +28,7 @@ class TipsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setOnBoardingItems()
+        prepareBiometric()
         textSkip.setOnClickListener {
           findNavController().navigate(R.id.homeFragment)
         }
@@ -64,6 +72,58 @@ class TipsFragment : Fragment() {
         }
         else{
 
+        }
+
+    }
+    private fun prepareBiometric(){
+        val biometricManager = BiometricManager.from(activity!!.applicationContext)
+
+        //Some more information on biometric support
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                Log.d("MY_APP_TAG", "App can authenticate using biometrics.")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Log.e("MY_APP_TAG", "No biometric features available on this device.")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                Log.e("MY_APP_TAG", "The user hasn't associated " +
+                        "any biometric credentials with their account.")
+        }
+
+        if (biometricManager.canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS){
+            Log.d("MainActivity","Device supports biometric auth")
+
+            val executor = ContextCompat.getMainExecutor(activity!!.applicationContext)
+
+            val callback = object: BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Log.d("MainActivity", "$errorCode :: $errString")
+                    if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
+                        activity!!.finish()
+                    }
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Log.d("MainActivity","Authentication failed")
+                }
+
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    Log.d("MainActivity","Authentication was successful")
+                }
+            }
+
+            val biometricPrompt = BiometricPrompt(this, executor, callback)
+            val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setDescription("User needs to be authenticated before using the app just choose the method you want to get started")
+                .setDeviceCredentialAllowed(true)
+                .build()
+
+            biometricPrompt.authenticate(promptInfo)
         }
 
     }
